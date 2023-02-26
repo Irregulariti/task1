e31d03b18633f3833b0acfc75aa42077b68bb3bd
irregulariti1 <ermolin.aa@edu.spbstu.ru>
2023-02-26 04:01:43 +0300

    rep

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+
+import static com.google.common.math.IntMath.mod;
 
 public class Task1 {
 
@@ -9,23 +12,26 @@ public class Task1 {
 
     public String plus(Task1 other) {
         String num1, num2;
-        if (this.number.toString().length() >= other.number.toString().length()) {
-            num1 = this.number.toString();
-            num2 = other.number.toString();
+        if (this.number.length() >= other.number.length()) {
+            num1 = this.number;
+            num2 = other.number;
         } else {
-            num2 = this.number.toString();
-            num1 = other.number.toString();
+            num2 = this.number;
+            num1 = other.number;
         }
-        String answer = "0".repeat((num1.length()+1));
         num2 += "0".repeat(num1.length() - num2.length());
-        System.out.printf(answer);
-        for (int i = num1.length(); i > 0; i--) {
-            int temp = Integer.parseInt(String.valueOf(num2.charAt(i))) + Integer.parseInt(String.valueOf(num1.charAt(i)));
+        String answer = "0".repeat(num1.length() + 1);
+        int m = num1.length() - 1;
+        int c = 0;
+        int temp = 0;
+        for (int i = num1.length(); i >= 0; i--) {
+            temp = (num1.charAt(i) + '0') + (num2.charAt(i) + '0') + c;
+            c = temp/10;
+            temp = mod(temp,10);
 
         }
         return "";
     }
-
     @Override
     public String toString() {
         return number;
