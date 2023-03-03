654103f7bba2130446d717043b5beae2fa5bb453
irregulariti1 <ermolin.aa@edu.spbstu.ru>
2023-03-03 03:16:45 +0300

    first finally commit

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -3,25 +3,40 @@ import java.util.ArrayList;
 
 import static com.google.common.math.IntMath.mod;
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
 
 public class Task1 {
 
-    private final String number;
+    private final String baidjan;
 
     public Task1(int precision, String string) {
-        this.number = string.substring(0, string.length() - precision) + "," + string.substring(string.length() - precision);
+        if (precision != 0 && precision < string.length()) {
+            string = string.substring(0, string.length() - precision) + "," + string.substring(string.length() - precision);
+            while (string.charAt(string.length() - 1) == '0' || string.charAt(string.length() - 1) == ',') {
+                if (string.charAt(string.length() - 1) == '0') {
+                    string = string.substring(0, string.length() - 1);
+                } else {
+                    string += "0";
+                    break;
+                }
+            }
+            this.baidjan = string;
+        } else if (precision >= string.length()) {
+            string = "0" + "," + "0".repeat(precision - string.length()) + string;
+            this.baidjan = string;
+        } else {
+            this.baidjan = string + ",0";
+        }
     }
 
-    public String plus(Task1 other) {
+    public Task1 plus(Task1 other) {
         String num1, num2;
-        if (this.number.length() >= other.number.length()) {
-            num1 = this.number;
-            num2 = other.number;
-        } else {
-            num2 = this.number;
-            num1 = other.number;
-        }
-        num2 = "0".repeat(num1.length() - num2.length()) + num2;
+        num1 = this.baidjan;
+        num2 = other.baidjan;
+        num1 = "0".repeat(Math.max(num2.substring(0, num2.indexOf(',')).length() - num1.substring(0, num1.indexOf(',')).length(), 0)) + num1;
+        num2 = "0".repeat(Math.max(num1.substring(0, num1.indexOf(',')).length() - num2.substring(0, num2.indexOf(',')).length(), 0)) + num2;
+        num1 += "0".repeat(Math.max(0, num2.substring(num2.indexOf(',') + 1).length() - num1.substring(num1.indexOf(',') + 1).length()));
+        num2 += "0".repeat(Math.max(0, num1.substring(num1.indexOf(',') + 1).length() - num2.substring(num2.indexOf(',') + 1).length()));
         String answer = "0".repeat(num1.length());
         int c = 0;
         int temp = 0;
@@ -42,18 +57,28 @@ public class Task1 {
             if (answer.charAt(answer.length() - 1) == '0') {
                 answer = answer.substring(0, answer.length() - 1);
             } else {
-                answer = answer.substring(0, answer.length() - 1);
+                answer += "0";
                 break;
             }
         }
-        return answer;
+        while (answer.charAt(0) == '0' && answer != "0"){
+            answer = answer.substring(1);
+        }
+        if (answer.indexOf(',') != -1) {
+            return new Task1(answer.length() - answer.indexOf(',') - 1, answer.substring(0, answer.indexOf(',')) + answer.substring(answer.indexOf(',') + 1));
+        } else {
+            return new Task1(0, answer);
+        }
     }
 
-    public String minus(Task1 other) {
+    public Task1 minus(Task1 other) {
         String num1, num2;
-        num1 = this.number;
-        num2 = other.number;
-        num2 = "0".repeat(num1.length() - num2.length()) + num2;
+        num1 = this.baidjan;
+        num2 = other.baidjan;
+        num1 = "0".repeat(Math.max(num2.substring(0, num2.indexOf(',')).length() - num1.substring(0, num1.indexOf(',')).length(), 0)) + num1;
+        num2 = "0".repeat(Math.max(num1.substring(0, num1.indexOf(',')).length() - num2.substring(0, num2.indexOf(',')).length(), 0)) + num2;
+        num1 += "0".repeat(Math.max(0, num2.substring(num2.indexOf(',') + 1).length() - num1.substring(num1.indexOf(',') + 1).length()));
+        num2 += "0".repeat(Math.max(0, num1.substring(num1.indexOf(',') + 1).length() - num2.substring(num2.indexOf(',') + 1).length()));
         String answer = "0".repeat(num1.length());
         int c = 0;
         int temp = 0;
@@ -74,23 +99,27 @@ public class Task1 {
             if (answer.charAt(answer.length() - 1) == '0') {
                 answer = answer.substring(0, answer.length() - 1);
             } else {
-                answer = answer.substring(0, answer.length() - 1);
+                answer += "0";
                 break;
             }
         }
-        return answer;
+        if (answer.indexOf(',') != -1) {
+            return new Task1(answer.length() - answer.indexOf(',') - 1, answer.substring(0, answer.indexOf(',')) + answer.substring(answer.indexOf(',') + 1));
+        } else {
+            return new Task1(0, answer);
+        }
     }
 
     public Task1 multiply(Task1 other) {
         String num1, num2;
-        if (this.number.length() >= other.number.length()) {
-            num1 = this.number;
-            num2 = other.number;
+        if (this.baidjan.length() >= other.baidjan.length()) {
+            num1 = this.baidjan;
+            num2 = other.baidjan;
         } else {
-            num2 = this.number;
-            num1 = other.number;
+            num2 = this.baidjan;
+            num1 = other.baidjan;
         }
-        int comma = (num1.length() - num1.indexOf(',') - 1) + (num2.length() - num2.indexOf(',') - 1);
+        int comma = num1.substring(num1.indexOf(',') + 1).length() + num2.substring(num2.indexOf(',') + 1).length();
         num1 = num1.substring(0, num1.indexOf(',')) + num1.substring(num1.indexOf(',') + 1);
         num2 = num2.substring(0, num2.indexOf(',')) + num2.substring(num2.indexOf(',') + 1);
         String answer = "0".repeat(num1.length() + 1);
@@ -111,7 +140,7 @@ public class Task1 {
                 if (c != 0) {
                     answer = c + answer;
                 }
-                answer += "0".repeat(num1.length() - i - 1);
+                answer += "0".repeat(num2.length() - i - 1);
                 if (answer.length() <= comma) {
                     answer = "0".repeat(abs(answer.length() - comma) + 1) + answer;
                 }
@@ -120,25 +149,79 @@ public class Task1 {
             }
         }
         Task1 multi = process.get(0);
-        System.out.println(process);
-        for (int i = 1; i < process.size() ; i++) {
-            multi.plus(process.get(i));
+        for (int i = 1; i < process.size(); i++) {
+            multi = multi.plus(process.get(i));
         }
-        System.out.println(multi);
-//        while (answer.charAt(answer.length() - 1) == '0' || answer.charAt(answer.length() - 1) == ',') {
-//            if (answer.charAt(answer.length() - 1) == '0') {
-//                answer = answer.substring(0, answer.length() - 1);
-//            } else {
-//                answer = answer.substring(0, answer.length() - 1);
-//                break;
-//            }
-//        }
         return multi;
+    }
+
+    public Task1 division(Task1 other, int precision) {
+        String ans = "";
+        String num1 = this.baidjan;
+        String num2 = other.baidjan;
+        int c2 = num2.substring(num2.indexOf(',') + 1).length();
+        num2 = num2.substring(0, num2.indexOf(',')) + num2.substring(num2.indexOf(',') + 1);
+        int num = Integer.parseInt(num2);
+        while (c2 != 0) {
+            if (num1.indexOf(',') != num1.length()-1) {
+                num1 = num1.substring(0,num1.indexOf(',')) + num1.charAt(num1.indexOf(',') +  1) + ',' + num1.substring(num1.indexOf(',') + 2);
+            } else {
+                num1 = num1.substring(0,num1.indexOf(',')) + "0,0";
+            }
+            c2--;
+        }
+        int i = 0;
+        int comma = 0;
+        String temp = "";
+        Boolean flag = false;
+        while (true) {
+            if (i != num1.length()) {
+                if (num1.charAt(i) == ',') {
+                    comma = ans.length();
+                    flag = true;
+                    i++;
+                    continue;
+                }
+                temp += num1.charAt(i);
+                i++;
+            } else {
+                temp += "0";
+            };
+            int tempInt = Integer.parseInt(temp);
+            if (tempInt / num != 0) {
+                ans += String.valueOf(tempInt/num);
+                temp = String.valueOf(tempInt%num);
+            } else {
+                if (ans.length() != 0) {
+                    ans += "0";
+                }
+            }
+            if (flag && (ans.substring(comma).length() == precision + 1 || tempInt == 0)){
+                break;
+            }
+        }
+        return new Task1(ans.length()-comma, ans).round(precision);
+    }
+
+    public Task1 round(int precision) {
+        String num = this.baidjan;
+        String temp = num.substring(num.indexOf(',') + 1);
+        String answer = num.substring(0, num.indexOf(','));
+        if (temp.length() <= precision) {
+            return this;
+        } else {
+            if (Character.getNumericValue(temp.charAt(precision)) >= 5) {
+                temp = temp.substring(0, precision - 1) + (Character.getNumericValue(temp.charAt(precision - 1)) + 1);
+            } else {
+                temp = temp.substring(0, precision - 1) + (Character.getNumericValue(temp.charAt(precision - 1)));
+            }
+        }
+        return new Task1(precision, answer + temp);
     }
 
     @Override
     public String toString() {
-        return number;
+        return baidjan;
     }
 
 }
--- src/main/java/Task1Test.java
+++ src/main/java/Task1Test.java
@@ -1,18 +1,52 @@
 
-import java.io.*;
-
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class Task1Test {
 
+    Task1 num1 = new Task1(2, "3333");
+    Task1 num2 = new Task1(3, "2555");
+    Task1 num3 = new Task1(6, "123456789");
+    Task1 num4 = new Task1(2, "12");
+    Task1 num5 = new Task1(1,"1");
+
+
     @Test
-    public void test() {
-        Task1 num1 = new Task1(1, "48");
-        Task1 num2 = new Task1(1, "75");
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num1.plus(num2));
-        System.out.println(num1.minus(num2));
-        System.out.println(num1.multiply(num2));
+    public void plusTest() {
+        assertEquals(num1.plus(num2).toString(), new Task1(3, "35885").toString());
+        assertEquals(num2.plus(num3).toString(), new Task1(6, "126011789").toString());
+        assertEquals(num3.plus(num4).toString(), new Task1(6, "123576789").toString());
+        assertEquals(num5.plus(num5).toString(), new Task1(1, "2").toString());
+    }
+
+    @Test
+    public void minusTest() {
+        assertEquals(num1.minus(num2).toString(), new Task1(3, "30775").toString());
+        assertEquals(num3.minus(num2).toString(), new Task1(6, "120901789").toString());
+        assertEquals(num2.minus(num4).toString(), new Task1(3, "2435").toString());
+    }
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(num1.multiply(num2).toString(), new Task1(5, "8515815").toString());
+        assertEquals(num3.multiply(num4).toString(), new Task1(8, "1481481468").toString());
+        assertEquals(num2.multiply(num4).toString(), new Task1(4, "3066").toString());
+    }
+
+    @Test
+    public void divisionTest(){
+        assertEquals(num1.division(num2, 3).toString(), new Task1(3, "13045").toString());
+        assertEquals(num3.division(num4, 3).toString(), new Task1(3, "1028807").toString());
+        assertEquals(num2.division(num4, 3).toString(), new Task1(3, "21292").toString());
+    }
+
+    @Test
+    public void roundTest(){
+        assertEquals(num1.round(1).toString(), new Task1(1, "333").toString());
+        assertEquals(num2.round(2).toString(), new Task1(2, "256").toString());
+        assertEquals(num3.round(3).toString(), new Task1(3, "123457").toString());
+        assertEquals(num4.round(1).toString(), new Task1(1, "1").toString());
     }
 
 }
